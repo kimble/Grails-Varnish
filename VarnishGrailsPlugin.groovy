9a72baaa2399c520d28c790a12304d7345b275a6
Kim A. Betti <kim.betti@gmail.com>
2011-06-21 13:52:42 +0200

    Updated plugin descriptor

--- VarnishGrailsPlugin.groovy
+++ VarnishGrailsPlugin.groovy
@@ -1,50 +1,26 @@
 class VarnishGrailsPlugin {
-    // the plugin version
+
     def version = "0.1"
-    // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.3.7 > *"
-    // the other plugins this plugin depends on
     def dependsOn = [:]
-    // resources that are excluded from plugin packaging
     def pluginExcludes = [
-            "grails-app/views/error.gsp"
+            "grails-app/views/*.gsp"
     ]
 
-    // TODO Fill in these fields
-    def author = "Your name"
-    def authorEmail = ""
-    def title = "Plugin summary/headline"
-    def description = '''\\
-Brief description of the plugin.
-'''
-
-    // URL to the plugin's documentation
-    def documentation = "http://grails.org/plugin/varnish"
-
-    def doWithWebDescriptor = { xml ->
-        // TODO Implement additions to web.xml (optional), this event occurs before 
-    }
+    def author = "Kim A. Betti"
+    def authorEmail = "kim@developer-b.com"
+    def title = "Varnish"
+    def description = "Simplified Varnish integration"
+    def documentation = "https://github.com/kimble/Grails-Varnish"
 
     def doWithSpring = {
         // TODO Implement runtime spring config (optional)
     }
-
-    def doWithDynamicMethods = { ctx ->
-        // TODO Implement registering dynamic methods to classes (optional)
-    }
-
-    def doWithApplicationContext = { applicationContext ->
-        // TODO Implement post initialization spring config (optional)
-    }
-
-    def onChange = { event ->
-        // TODO Implement code that is executed when any artefact that this plugin is
-        // watching is modified and reloaded. The event contains: event.source,
-        // event.application, event.manager, event.ctx, and event.plugin.
-    }
-
-    def onConfigChange = { event ->
-        // TODO Implement code that is executed when the project configuration changes.
-        // The event is the same as for 'onChange'.
-    }
+    
+    def doWithDynamicMethods = { ctx -> }
+    def doWithApplicationContext = { applicationContext -> }
+    def onChange = { event -> }
+    def onConfigChange = { event -> }
+    def doWithWebDescriptor = { xml -> }
+    
 }
